733fcb46e3ffc5c61b9495b59010b739eb6e2ea1
fabe <fabian.berg@adesso.de>
2017-05-17 14:28:42 +0200

    added minifcation & renamed output files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 "use strict";
 
 var gulp = require('gulp');
-var scssCompiler = require('gulp-sass');
+var scss = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
-gulp.task('scss', function () {
-    return gulp.src('./src/main.scss').pipe(scssCompiler()).pipe(gulp.dest('./dist/css'));
+gulp.task('transpile:scss', function () {
+    return gulp.src('./src/main.scss', {})
+        .pipe(rename('responsive-gaps'))
+        .pipe(scss()).pipe(gulp.dest('./dist/css'));
 });
+
+gulp.task('minify:css', ['transpile:scss'], function () {
+    gulp.src('./dist/css/responsive-gaps.css')
+        .pipe(cleanCss())
+        .pipe(rename('responsive-gaps.min.css'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('build', ['minify:css']);
+
+gulp.task('default', ['build']);
